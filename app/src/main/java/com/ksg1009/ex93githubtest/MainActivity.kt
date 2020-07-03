e5d71dd1a47ee45e0fe157c71b26cfe26066c972
ksg <ksg1645@gmail.com>
2020-07-03 14:34:27 +0900

    Initial commit

---
+++ app/src/main/java/com/ksg1009/ex93githubtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ksg1009.ex93githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
